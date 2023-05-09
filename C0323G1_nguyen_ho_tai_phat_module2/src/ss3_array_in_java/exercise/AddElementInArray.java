aba61efc05e505cc7830f6e156f0a1249f4891ed
taiphat99 <taiphatlqp@gmail.com>
2023-05-09 20:54:35 +0700

    a

---
+++ C0323G1_nguyen_ho_tai_phat_module2/src/ss3_array_in_java/exercise/AddElementInArray.java
@@ -0,0 +1,20 @@
+package ss3_array_in_java.exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddElementInArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 0, 0, 0, 0, 0, 0};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number u would like to add in the array");
+        int number = sc.nextInt();
+        System.out.println("Enter index u would like to add " + number + " in the array");
+        int index = sc.nextInt();
+        array[index] = number;
+        for (int i = index; i < array.length - 2; i++) {
+            array[i + 1] = array[i + 2];
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
