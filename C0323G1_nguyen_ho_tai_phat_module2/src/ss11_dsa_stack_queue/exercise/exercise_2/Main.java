933bd33b583e257c317ad82f6067ad85c734f7be
taiphat99 <taiphatlqp@gmail.com>
2023-05-19 21:50:30 +0700

    a

--- C0323G1_nguyen_ho_tai_phat_module2/src/ss11_dsa_stack_queue/exercise/exercise_2/Main.java
+++ C0323G1_nguyen_ho_tai_phat_module2/src/ss11_dsa_stack_queue/exercise/exercise_2/Main.java
@@ -1,4 +1,29 @@
 package ss11_dsa_stack_queue.exercise.exercise_2;
 
+import java.util.Stack;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(converter(30));
+        System.out.println(converter(41));
+        System.out.println(converter(31));
+        System.out.println(converter(5));
+    }
+    public static String converter(int number) {
+        Stack<Integer> stack = new Stack<>();
+        while (number / 2 != 0) {
+            stack.push(number % 2);
+            number = number / 2;
+            if(number == 1){
+                stack.push(1);
+            }
+        }
+        StringBuilder str = new StringBuilder();
+        int stackSize = stack.size();
+        for (int i = 0; i <stackSize; i++) {
+            str.append(stack.pop());
+        }
+
+        return str.toString();
+    }
 }
